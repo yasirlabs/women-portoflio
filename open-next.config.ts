247735b04428b1bbc18ca6d2c3971b229f2bb3c4
yasirlabs <mubermij@gmail.com>
2026-03-06 00:58:58 +0300

    Add open-next.config.ts for Cloudflare deployment

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,3 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-import kvIncrementalCache from "@opennextjs/cloudflare/kv-cache";
 
-export default defineCloudflareConfig({
-  incrementalCache: kvIncrementalCache,
-});
+export default defineCloudflareConfig({});
